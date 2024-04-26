081e628ca365c3bfb06801692e178a84016cdf36
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-04-26 16:52:15 +0900

    basic crud

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductListAPIView.as_view())
+    path('', views.ProductListAPIView.as_view()),
+    path('<int:product_id>/', views.ProductDetailAPIView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
+from products.models import Product
+from products.serializers import ProductSerializer
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
 
 class ProductListAPIView(APIView):
     def get(self, request):
-        pass
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        return Response(serializer.data)
+
+
+class ProductDetailAPIView(APIView):
+    def get_object(self, product_id):
+        return get_object_or_404(Product, id=product_id)
+
+    def put(self, request, product_id):
+        product = self.get_object(product_id)
+        serializer = ProductSerializer(data=request.data, instance=product)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        
+    def delete(self, request, product_id):
+        product = self.get_object(product_id)
+        product.delete()
+        return Response(f"{product_id}번 상품 삭제완료")
