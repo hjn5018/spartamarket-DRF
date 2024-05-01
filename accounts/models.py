14ea413433881dff1e16e1dcf05a19526afc3a3d
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-05-01 14:07:04 +0900

    페이지네이션 마무리

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    # birth_date = models.DateTimeField()
-    nickname = models.CharField(max_length=20)
+    gender_choices = {
+        
+    }
+
+
+    birth_date = models.DateTimeField(null=True)
+    nickname = models.CharField(max_length=20, blank=True)
+    gender = models.CharField()
